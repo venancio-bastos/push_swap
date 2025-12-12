e0cb64817e873c8774f5722b361fd4f518a1e889
venancio-bastos <vjpb2002@gmail.com>
2025-12-12 01:07:23 +0000

    push updates

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: vebastos <vebastos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 13:10:56 by vebastos          #+#    #+#             */
-/*   Updated: 2025/12/09 13:29:51 by vebastos         ###   ########.fr       */
+/*   Updated: 2025/12/09 19:47:27 by vebastos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,9 @@ int	ft_atoi(char *c)
 	while (c[i])
 		result = (result * 10) + (c[i++] - '0');
 	return (result);
+}
+
+char ft_split(char arr, char sep)
+{
+	int	i;
 }
